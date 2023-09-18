e88f8a94455123c70db83bf3c44092341698c418
Rahul Desai <desairahul352@gmail.com>
2023-09-18 19:46:55 +0530

    added more controller and routes i.e about

---
+++ controllers/about_controller.js
@@ -0,0 +1,3 @@
+module.exports.about  = function(req,res){
+    res.end('<h1>about codeial!</h1>')
+}
--- routes/index.js
+++ routes/index.js
@@ -6,5 +6,6 @@ const homeController = require('../controllers/home_controller');
 console.log('router loaded')
 
 router.get('/',homeController.home);
-
+router.use('/users', require('./users'));
+router.use('/users',require('./about'));
 module.exports = router;
