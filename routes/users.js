460d06793d26f256188c01e7b6d27962438cedf3
Rahul Desai <desairahul352@gmail.com>
2023-09-25 12:12:53 +0530

    created sign up for user

--- routes/users.js
+++ routes/users.js
@@ -5,6 +5,10 @@ const router = express.Router();
 const usersController = require('../controllers/user_controller');
 router.get('/profile',usersController.profile);
 
+router.get('/sign-up',usersController.signUp);
+router.get('/sign-in',usersController.signIn);
+
+router.post('/create',usersController.create);
 
 
 module.exports = router;
