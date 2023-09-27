71136e5155586714516cb74459c61c07895993e6
Rahul Desai <desairahul352@gmail.com>
2023-09-27 19:14:10 +0530

    created postSchema

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -8,6 +8,10 @@ module.exports.profile = function(req,res){
 
 //render the sign up page
 module.exports.signUp = function(req,res){
+    if(req.isAuthenticated()){
+        return res.redirect('/users/profile');
+    }
+
     return res.render ('user_sign_up',{
         title:"Codeial | Sign Up"
     })
@@ -15,6 +19,10 @@ module.exports.signUp = function(req,res){
 
 //render the sign in page
 module.exports.signIn = function(req,res){
+    if(req.isAuthenticated()){
+        return res.redirect('/users/profile');
+    }
+
     return res.render ('user_sign_in',{
         title:"Codeial | Sign In"
     })
@@ -43,5 +51,12 @@ module.exports.create = function(req,res){
  
 //sign in and create a session for the user 
 module.exports.createSession = function(req,res){
-    //TODO later
+     return res.redirect('/');
 } 
+
+module.exports.destroySession = function(req, res, next) {
+    req.logout(function(err) {
+      if (err) { return next(err); }
+      res.redirect('/');
+    });
+  };
